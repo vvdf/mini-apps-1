b5aae81e7012cc20bb72bd4a01b7c91f2c44dbed
7FFF00 <t42please@gmail.com>
2020-01-14 11:43:19 -0800

    IMPLEMENT basic page and server structure

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,28 @@
+var select = (domElement) => document.getElementById(domElement);
+var create = (domElement) => document.createElement(domElement);
 
+var init = () => {
+  var jsonSubmit = select('JSONsubmit');
+  jsonSubmit.addEventListener('click', (event) => {
+    // take input from json input box
+    var inputBox = select('JSONinput');
+    var inputText = JSON.stringify(inputBox.value);
+    // inputBox.value = ''; // clear input box
+    console.log(inputText, " cached to be sent");
 
+    // call to POST via fetch, taking in USVstring endpoint and options
+    fetch('/api/JSONconvert', {
+      method: 'POST',
+      body: inputText,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        // output converted data to csv output box
+        console.log(response);
+      });
+  });
+};
+
+init();
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = 3000;
 
+app.get('/', (req, res, next) => {
+  console.log(`${req.method} request at url ${req.url}`);
+  next();
+});
 
+app.post('/api/JSONconvert', (req, res) => {
+  console.log(`${req.method} request at url ${req.url}`);
+  console.log(req.body);
+  res.send('JSON received');
+});
+
+app.use('/', express.static('client'));
+
+app.listen(PORT, () => {
+  console.log('Server listening on port:', PORT);
+});
