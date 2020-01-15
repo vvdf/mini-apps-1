faf0015fe82fa437d3e821c8a41b7168b3952715
7FFF00 <t42please@gmail.com>
2020-01-15 11:19:25 -0800

    INITIAL setup of node server and html page

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} @ ${req.url}`);
+  next();
+});
+
+app.use(express.static('public'));
+
+app.listen(port);
