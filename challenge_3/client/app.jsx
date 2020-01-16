1e527418714bd9747deb9dcc34d44436e952d085
7FFF00 <t42please@gmail.com>
2020-01-15 17:58:48 -0800

    IMPLEMENT basic React component and page rendering functionality

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,65 @@
+var divStyle = {
+  display: 'flex',
+  justifyContent: 'center'
+};
+
+var formStyle = { 
+  width: '50%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignContent: 'center'
+};
+
+var formEntryStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+};
+
+var buttonStyle = {
+  width: '30%',
+};
+
+var UserInputs = ['name', 'email', 'password'];
+var AddressInputs = ['address line 1', 'address line 2', 'city', 'state', 'zip', 'phone'];
+var PaymentInputs = ['credit card number', 'expiry date', 'cvv', 'billing zip code'];
+
+var CheckoutForm = (props) => {
+  return (
+      <div style={divStyle}>
+        <form style={formStyle}>
+            <label style={formEntryStyle}>
+              Name:
+              <input type="text" placeholder="name" />
+            </label>
+            <label style={formEntryStyle}>
+              Email:
+              <input type="text" placeholder="email@address.com" />
+            </label>
+            <label style={formEntryStyle}>
+              Password:
+              <input type="text" placeholder="password" />
+            </label>
+          <input type="submit" value="Next Step" style={buttonStyle} />
+        </form>
+      </div>
+    );
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+          <CheckoutForm />
+        </div>
+      );
+  }
+}
 
 
+// render to DOM
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ challenge_3/server/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'checkout'
+});
+
+connection.connect();
+
+
+// database file server/checkout_db.db, tables are USERS and TRANSACTIONS
+// USER columns: id, name, email, password
+// TRANSACTION columns: id user_id address1 address2 city state zip phone 
+//  ccn exp cvv billing_zip
+
+connection.query('SHOW tables', (err, results, fields) => {
+  console.log(results);
+});
+
